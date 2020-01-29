0871be7cb9b4c89b77b23b7431faa1f0e7f0dfb8
Hoshiyar Singh <hoshiyar.mehra9@gmail.com>
2020-01-29 21:51:57 +0530

    Sql Database project is added

---
+++ Ng_Prayas.Db/Pre_Deployment_Scripts.sql
@@ -0,0 +1,126 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+USE [Ngo_Prayas_DB]
+GO
+ALTER TABLE [dbo].[Users] DROP CONSTRAINT [FK__Users__RoleId__398D8EEE]
+GO
+ALTER TABLE [dbo].[Events] DROP CONSTRAINT [FK__Events__Category__3F466844]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 1/29/2020 9:51:23 PM ******/
+DROP TABLE [dbo].[Users]
+GO
+/****** Object:  Table [dbo].[Roles]    Script Date: 1/29/2020 9:51:23 PM ******/
+DROP TABLE [dbo].[Roles]
+GO
+/****** Object:  Table [dbo].[Events]    Script Date: 1/29/2020 9:51:23 PM ******/
+DROP TABLE [dbo].[Events]
+GO
+/****** Object:  Table [dbo].[EventCategories]    Script Date: 1/29/2020 9:51:23 PM ******/
+DROP TABLE [dbo].[EventCategories]
+GO
+/****** Object:  Table [dbo].[ContactUs]    Script Date: 1/29/2020 9:51:23 PM ******/
+DROP TABLE [dbo].[ContactUs]
+GO
+/****** Object:  Table [dbo].[ContactUs]    Script Date: 1/29/2020 9:51:23 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ContactUs](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[EmailId] [nvarchar](50) NOT NULL,
+	[MobileNumber] [nvarchar](20) NOT NULL,
+	[Description] [nvarchar](1000) NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[CreatedDate] [datetime2](7) NOT NULL,
+	[ModifiedDate] [datetime2](7) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[EventCategories]    Script Date: 1/29/2020 9:51:23 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[EventCategories](
+	[CategoryId] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [nvarchar](260) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[CategoryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Events]    Script Date: 1/29/2020 9:51:23 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Events](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[EventName] [nvarchar](250) NOT NULL,
+	[EventDescription] [nvarchar](1000) NOT NULL,
+	[EventDate] [datetime2](7) NOT NULL,
+	[EventStartTime] [nvarchar](10) NOT NULL,
+	[EventEndTime] [nvarchar](10) NOT NULL,
+	[CreatedDate] [datetime2](7) NOT NULL,
+	[ModifiedDate] [datetime2](7) NULL,
+	[IsActive] [bit] NOT NULL,
+	[NoOfClicks] [int] NULL,
+	[EventLocation] [nvarchar](100) NOT NULL,
+	[ImagePath] [nvarchar](250) NOT NULL,
+	[CategoryId] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Roles]    Script Date: 1/29/2020 9:51:23 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Roles](
+	[Id] [int] NOT NULL,
+	[RoleName] [varchar](50) NOT NULL,
+	[IsActive] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 1/29/2020 9:51:23 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[Id] [int] NOT NULL,
+	[EmailId] [varchar](50) NOT NULL,
+	[Password] [nvarchar](50) NOT NULL,
+	[IsActive] [bit] NULL,
+	[CreatedDate] [datetime] NULL,
+	[RoleId] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Events]  WITH CHECK ADD FOREIGN KEY([CategoryId])
+REFERENCES [dbo].[EventCategories] ([CategoryId])
+GO
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Roles] ([Id])
+GO
