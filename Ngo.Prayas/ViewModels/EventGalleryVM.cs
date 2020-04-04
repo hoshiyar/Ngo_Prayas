5d2aa11ef3363fba1dd7fad4817e4da2f2d5de0a
Hoshiyar Singh <hoshiyar.mehra9@gmail.com>
2020-04-04 13:09:22 +0530

    asd

---
+++ Ngo.Prayas/ViewModels/EventGalleryVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ngo.Prayas.ViewModels
+{
+    public class EventGalleryVM
+    {
+        public int Id { get; set; }
+
+        public string ImageName { get; set; }
+
+        [Required]
+        public HttpPostedFileBase[] Files { get; set; }
+
+        [Required]
+        public string GalleryName { get; set; }
+
+        [Required]
+        public string GalleryDescription { get; set; }
+    }
+
+    public class PagedList<T>
+    {
+        public List<T> Content { get; set; }
+        public Int32 CurrentPage { get; set; }
+        public Int32 PageSize { get; set; }
+        public int TotalRecords { get; set; }
+
+    }
+
+}
